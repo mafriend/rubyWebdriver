fa5db2b8a0b6b686b362f5316b8942e960d50c9c
mafriend <monicaafriend@gmail.com>
2021-03-12 12:00:24 -0600

    added test as a challenge to use the date picker itself to choose a date yo

--- webdriver-ruby-practice/component_testing.rb
+++ webdriver-ruby-practice/component_testing.rb
@@ -6,10 +6,17 @@ driver.navigate.to "https://formy-project.herokuapp.com/datepicker"
 # few different ways to automate a datepicker: either send keys with a date or actually select the date from the picker itself
 
 #send keys option
-datefeild = driver.find_element(id: "datepicker")
-datefeild.send_keys("10/18/2021")
-datefeild.send_keys :return
+datefield = driver.find_element(id: "datepicker")
+# datefield.send_keys("10/18/2021")
+# datefield.send_keys :return
 
 #why cant i just send keys to hit enter? the date picker is over the original container and selenium doesnt like that so you have to use :return instead of sending keys
 
-# Homework: actually USE the date picker
+# Homework: actually USE and find the date with the datepicker
+datefield.click()
+
+
+datePicked = driver.find_element(css: '[input: data-date = "1618012800000"]')
+datePicked.click()
+
+#maybe it would be better to use the first example to find specific dates and the second example to find a date just in general
